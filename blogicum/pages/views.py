6255309cb581e989b3de1622776a84be46b02104
shyravina19102004 <shyravina19102004@gmail.com>
2025-12-14 18:41:08 +0300

    docstring1

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,11 +1,14 @@
+"""Views для приложения pages."""
 from django.shortcuts import render
 
 
 def about(request):
+    """Отображает страницу 'О проекте'."""
     template = 'pages/about.html'
     return render(request, template)
 
 
 def rules(request):
+    """Отображает страницу 'Правила'."""
     template = 'pages/rules.html'
     return render(request, template)
